ad0e5e60e8bffc4b8324bc4a315a861f32ebb36a
chipopo <carbbykid@gmail.com>
2022-10-31 02:20:44 +0700

    feat: add link first project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,13 +27,18 @@ const Portfolio = () => {
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
-                alt=""
+                alt="chipo"
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href="https://chipo-dashboard-uigo.vercel.app/"
+                  alt="chipo"
+                  target="_blank"
+                  className=" px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   Demo
-                </button>
+                </a>
                 {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
                 </button> */}

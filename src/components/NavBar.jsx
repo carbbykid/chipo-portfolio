880e387df055cdecf8258898146fe5fa3b10f528
chipopo <carbbykid@gmail.com>
2022-10-30 17:34:12 +0700

    fix: nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
 
   const links = [
     {
@@ -28,8 +30,19 @@ const NavBar = () => {
     },
   ];
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 10) setIsScroll(true);
+      else setIsScroll(false);
+    });
+  }, [isScroll]);
+
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div
+      className={`flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed shadow-sm duration-200 ${
+        isScroll && "shadow-white bg-opacity-80"
+      }`}
+    >
       <div>
         <h1 className="text-5xl font-signature ml-2">ChiPoPo</h1>
       </div>
@@ -38,7 +51,9 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className={`px-4 cursor-pointer capitalize font-medium hover:scale-105 duration-200 ${
+              isScroll ? "text-white" : "text-gray-500"
+            }`}
           >
             <Link to={link} smooth duration={500}>
               {link}
